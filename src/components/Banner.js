d807a05d072f9422c65de126a565d00f425c6e8a
Dheeraj singh <47826524+Dheerajsingh0@users.noreply.github.com>
2021-06-05 12:19:46 +0530

    family man2 prime link updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,8 @@ function Banner() {
             <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20"/>
             <Carousel autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={5000}>
             <div>
-                <img loading="lazy" src="/img/fm2.jpg" alt="slideimg1" />
+                <a href="https://www.primevideo.com/detail/The-Family-Man/0H3DDB4KBJFNDCKKLHNRLRLVKQ">
+                <img loading="lazy" src="/img/fm2.jpg" alt="slideimg1" /></a>
             </div>
             <div>
                 <img loading="lazy" src="/img/slide1.jpg" alt="slideimg1" />
