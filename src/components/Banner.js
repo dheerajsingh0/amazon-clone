cfd2f2b34850f7b128c6e4f55e90508e0e03e55c
Dheeraj singh <47826524+Dheerajsingh0@users.noreply.github.com>
2021-06-05 12:08:36 +0530

    updated family man 2 poster

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,6 +6,9 @@ function Banner() {
             <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20"/>
             <Carousel autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={5000}>
             <div>
+                <img loading="lazy" src="/img/fm2.jpg" alt="slideimg1" />
+            </div>
+            <div>
                 <img loading="lazy" src="/img/slide1.jpg" alt="slideimg1" />
             </div>
             <div>
