f02fb0ebb72444ab272d70e20bff9808725e0714
Dheeraj singh <47826524+dheerajsingh0@users.noreply.github.com>
2021-08-15 13:00:22 +0530

    added progess bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,20 @@ import Header from '../components/Header'
 import '../styles/globals.css'
 import {Provider as AuthProvider} from "next-auth/client";
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router"
+
+const progress=new ProgressBar({
+  size:8,
+  color:"#FF9900",
+  className:"z-50",
+
+});
+Router.events.on("routeChangeStart",progress.start);
+Router.events.on("routeChangeComplete",progress.finish);
+Router.events.on("routeChangeError",progress.finish);
+
+
 const MyApp = ({ Component, pageProps }) => {
   
   return (
